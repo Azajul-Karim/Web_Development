8b3b66aa23b94d8900e3e09cf65405f4bd75f9ee
Azajul Karim Topu <azajulkarim2021@gmail.com>
2024-08-30 06:50:30 +0600

    rest added

---
+++ Backend/rest/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+let posts = [
+  {
+    username: 'John',    
+    content : 'i love to code'
+  },
+  {
+    username: 'Jane',    
+    content : 'i love to play'
+  },
+  {
+    username: 'Rahi',    
+    content : 'i love to sleep'
+  },
+  {
+    username: 'Nazim',    
+    content : 'i love to eat'
+  }
+];
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+
+app.listen(port, () => {
+  console.log(`Server is ru nning on port ${port}`);
+});
